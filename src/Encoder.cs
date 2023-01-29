bef3b9fb47030818b8b02e7b5bd08cedcb730244
Christopher M Green <cgvoice35@gmail.com>
2023-01-29 15:28:03 -0600

    Initial files

---
+++ src/Encoder.cs
@@ -0,0 +1,11 @@
+﻿namespace PitchConverter
+{
+    public class Encoder
+    {
+        public Encoder() { }
+        public string GetTestMessage()
+        {
+            return "Hello, Read2Read app!";
+        }
+    }
+}
